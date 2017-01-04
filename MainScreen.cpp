84ebbd4bfc39377d752fe1dfb98b066b42aed2b4
TejaswiniSoor <tejusoor@gmail.com>
2017-01-04 10:52:30 +0530

    Add files via upload

--- MainScreen.cpp
+++ MainScreen.cpp
@@ -13,9 +13,41 @@ Widget::Widget(QWidget *parent) :
     QRect r = this->geometry();
     r.moveCenter(QApplication::desktop()->availableGeometry().center());
     this->setGeometry(r);
-}
+    ui->smallButton->setIcon(QIcon(":/small.bmp"));
+    ui->closeButton->setIcon(QIcon(":/close.bmp"));
+    ui->VersionLabel->setPixmap(QPixmap (":/Version.png"));
+    ui->smallButton->installEventFilter(this);
+    ui->closeButton->installEventFilter(this);
 
+    ui->label->setStyleSheet("QLabel { background-color : red; color : blue; }");
+   // ui->label_2->setStyleSheet("QLabel { background-color : blue; color : red; }");
+
+}
 Widget::~Widget()
 {
     delete ui;
+}
+bool Widget::eventFilter(QObject *obj, QEvent *event)
+{
+    if (obj == (QObject*)ui->smallButton || obj == (QObject*)ui->closeButton) {
+        if (event->type() == QEvent::Enter)
+        {
+            if(obj == (QObject*)ui->smallButton)
+                ui->smallButton->setIcon(QIcon(":/small-select.bmp"));
+            else if(obj == (QObject*)ui->closeButton)
+                ui->closeButton->setIcon(QIcon(":/close-select.bmp"));
+
+        }else if(event->type() == QEvent::Leave)
+        {
+            if(obj == (QObject*)ui->smallButton)
+                ui->smallButton->setIcon(QIcon(":/small.bmp"));
+            else if(obj == (QObject*)ui->closeButton)
+                ui->closeButton->setIcon(QIcon(":/close.bmp"));
+
+        }
+        return QWidget::eventFilter(obj, event);
+    }else {
+        // pass the event on to the parent class
+        return QWidget::eventFilter(obj, event);
+    }
 }
--- MainScreen.h
+++ MainScreen.h
@@ -10,10 +10,14 @@ class Widget;
 class Widget : public QWidget
 {
     Q_OBJECT
+public slots:
 
 public:
     explicit Widget(QWidget *parent = 0);
     ~Widget();
+    bool eventFilter(QObject *obj, QEvent *event);
+
+private slots:
 
 private:
     Ui::Widget *ui;
